002a188f4d6a145d024d05a7eda096b3bd035e3a
DenisShylov <dodjer123@gmail.com>
2022-02-07 14:31:08 +0200

    1

--- loops/task5/index.js
+++ loops/task5/index.js
@@ -4,9 +4,7 @@ let result = 0;
 for (let i = start; i <= end; i++) {
   if (i % 5 === 0) {
     console.log(i);
-  }
-
-  if (i % 2 === 0 && i % 4 !== 0) {
+  } else if (i % 2 === 0 && i % 4 !== 0) {
     result += i;
   }
 
