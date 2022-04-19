a349eaa956d9fc776b802d69a090e8cd75a23fad
DenisShylov <dodjer123@gmail.com>
2022-04-19 23:29:16 +0300

    up

--- Document-object-model(DOM)/task6/index.js
+++ Document-object-model(DOM)/task6/index.js
@@ -1,6 +1,6 @@
 const setButton = (buttonText) => {
   document.querySelector('body').innerHTML = buttonText;
-  // document.querySelector('body').textContent = buttonText;
+  document.querySelector('body').textContent = buttonText;
 };
 
 setButton();
