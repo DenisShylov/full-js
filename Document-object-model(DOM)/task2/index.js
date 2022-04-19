40301df6b35717e2e757a03e3a898370137dd832
DenisShylov <dodjer123@gmail.com>
2022-04-19 21:41:18 +0300

    up

--- Document-object-model(DOM)/task2/index.js
+++ Document-object-model(DOM)/task2/index.js
@@ -1,9 +1,12 @@
 export const getTitleElement = () => {
-  const titleElem = document.querySelector('.title');
-  console.dir(titleElem);
+  console.dir(document.querySelector('.title'));
+  return document.querySelector('.title');
 };
 
 export const getInputElement = () => {
   const inputElem = document.querySelector('input');
   console.dir(inputElem);
+  return inputElem;
 };
+// console.log(getInputElement());
+// console.log(getTitleElement());
