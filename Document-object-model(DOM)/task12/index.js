3b032b3e05e3e2016fbb5038bc4a88c440fa2155
DenisShylov <dodjer123@gmail.com>
2022-04-20 20:59:32 +0300

    upd

--- Document-object-model(DOM)/task12/index.js
+++ Document-object-model(DOM)/task12/index.js
@@ -1,7 +1,7 @@
 const getSection = (num) => {
   const search = document.querySelector(`span[data-number="${num}"]`);
   const parent = search.closest('.box');
-  return parent.dataset;
+  return parent.dataset.section;
 };
 // console.log(getSection(5));
 export { getSection };
